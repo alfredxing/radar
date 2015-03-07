791cda1e3bfd70cfca055e3abaad3b24ec37b276
Mimi Ko <mimiko.github@gmail.com>
2015-03-07 04:46:20 -0800

    Display weather data table in text

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,5 +2,12 @@ class MapController < ApplicationController
   def index
     curr = ActiveSupport::JSON.decode(Weather.first.current.to_json)
     @updated = curr["updated"]
+    @temperature = curr["temperature"]
+    @condition = curr["condition"]
+    @wind = curr["wind"]
+    @dewpoint = curr["dewpoint"]
+    @pressure = curr["pressure"]
+    @visibility = curr["visibility"] 
+    @humidity = curr["humidity"] 
   end
 end
