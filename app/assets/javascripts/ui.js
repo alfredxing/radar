alfredxing/radar

commit f2706de57d984017c631bbfdd1418ef95b6f17ec

Fix JSON AJAX bug

app/assets/javascripts/ui.js
@@ -18,13 +18,13 @@ function updateOptions() {
 $(window).load(function() {
     $("#refresh").click(function() {
         // Refresh radar
-        $.get("/data/radar.json", function(data) {
-            window.data = JSON.parse(data);
+        $.getJSON("/data/radar.json", function(res) {
+            window.data = res;
             overlay.draw();
         });
 
         // Refresh weather
-        $.get("/weather", function(data) {
+        $.getJSON("/weather", function(res) {
             $("#temp").text(data.current.temperature);
             $("#condition").text(data.current.condition);
             $("[data-tag=updated]").text((new Date(data.current.updated)).toLocaleString());

app/assets/javascripts/map.js
@@ -135,8 +135,8 @@ function initialize() {
         maxZoom: 10
     };
 
-    $.get("/data/radar.json", function(res) {
-        data = JSON.parse(res);
+    $.getJSON("/data/radar.json", function(res) {
+        data = res;
         mapOptions.center = { lat: 49.260605, lng: -123.245994};
         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
         overlay = new RadarOverlay(map);

commit beb5cc0758a35e0a57dd19575f660d313aafb164

Fix typo in weather refresh JS
Regression in f2706de57d984017c631bbfdd1418ef95b6f17ec

app/assets/javascripts/ui.js
@@ -25,13 +25,13 @@ $(window).load(function() {
 
         // Refresh weather
         $.getJSON("/weather", function(res) {
-            $("#temp").text(data.current.temperature);
-            $("#condition").text(data.current.condition);
-            $("[data-tag=updated]").text((new Date(data.current.updated)).toLocaleString());
+            $("#temp").text(res.current.temperature);
+            $("#condition").text(res.current.condition);
+            $("[data-tag=updated]").text((new Date(res.current.updated)).toLocaleString());
 
             for (var i = 0; i < DETAILED_WEATHER.length; i++) {
                 var tag = DETAILED_WEATHER[i];
-                $("[data-tag=" + tag + "]").text(data.current[tag]);
+                $("[data-tag=" + tag + "]").text(res.current[tag]);
             }
         });
 