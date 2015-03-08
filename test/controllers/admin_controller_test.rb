e5e3eb420fa2b05673a378282dc0073b66dcbd67
Alfred Xing <alfredxing@users.noreply.github.com>
2015-03-07 18:32:52 -0800

    Get existent tests passing

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
   test "should get panel" do
+    # Basic auth
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials("supersecretadminname", "supersecretadminpassword")
     get :panel
     assert_response :success
   end
