6b4404abcb0c8d71fda1d0bc8dcd98b54285ecc6
Imran Khan <imran-khan181@hotmail.com>
2015-03-27 15:21:13 -0700

    condensed code for displaying weather stations

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,9 @@
 class MapController < ApplicationController
   def index
-    curr = Weather.first.current
+    @prefs = user_signed_in? ? current_user.preferences : User::DEFAULT_PREFS
+    @w = Weather.find_by code: @prefs["code"]
+    curr = @w.current
+
     @updated = curr["updated"]
     @temperature = curr["temperature"]
     @condition = curr["condition"]
@@ -8,34 +11,7 @@ class MapController < ApplicationController
     @dewpoint = curr["dewpoint"]
     @pressure = curr["pressure"]
     @visibility = curr["visibility"]
-
     @humidity = curr["humidity"]
-    @prefs = user_signed_in? ? current_user.preferences : User::DEFAULT_PREFS
-
-
-    if @prefs["code"]=="s0000758"
-        @station = "abbotsford"
-    end
-    if @prefs["code"]=="s0000398"
-      @station = "Aggasiz"
-    end
-    if @prefs["code"]=="s0000323"
-      @station = "Squamish"
-    end
-    if @prefs["code"]=="s0000775"
-      @station = "Victoria"
-    end
-    if @prefs["code"]=="s0000373"
-      @station = "White Rock"
-    end
-    if @prefs["code"]=="s0000496"
-      @station = "Nanaimo"
-    end
-    if @prefs["code"]=="s0000078"
-      @station = "Whistler"
-    end
-    if @prefs["code"]=="s0000141"
-      @station = "Vancouver"
-    end
+    @station = @w.name
   end
   end
